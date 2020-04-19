80a968dcffb6f5964be89e3062281f3c782643fc
Iordanis Kostelidis <ikostelidis@jnksoftware.eu>
2020-04-19 20:53:51 +0300

    Updates

---
+++ precache-manifest.6c28ab8ad8a85fa82abab6bfc53db17a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77883d42bedf99f665b232d3c7c7455c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d51d75f4bd7146ffad7a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e3e8c298e8542340853",
+    "url": "/static/js/2.01b81494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.01b81494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51d75f4bd7146ffad7a",
+    "url": "/static/js/main.9b590027.chunk.js"
+  },
+  {
+    "revision": "79f8fe91c7d4464c64b3",
+    "url": "/static/js/runtime-main.88267c78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jnksoftware-portal/precache-manifest.91aa6fcdd95f74c24585b51cd5acfeb3.js"
+  "/precache-manifest.6c28ab8ad8a85fa82abab6bfc53db17a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jnksoftware-portal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
