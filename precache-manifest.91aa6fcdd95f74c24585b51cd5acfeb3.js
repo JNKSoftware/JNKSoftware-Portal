92b6b9211c707b855ece1cef8d1f2595a0705b76
Iordanis Kostelidis <ikostelidis@jnksoftware.eu>
2020-04-19 20:43:07 +0300

    Updates

---
+++ precache-manifest.91aa6fcdd95f74c24585b51cd5acfeb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b89ed5767db9bbd2466de26260df73",
+    "url": "/jnksoftware-portal/index.html"
+  },
+  {
+    "revision": "818a9dec76316d61941d",
+    "url": "/jnksoftware-portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4e3e8c298e8542340853",
+    "url": "/jnksoftware-portal/static/js/2.01b81494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jnksoftware-portal/static/js/2.01b81494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818a9dec76316d61941d",
+    "url": "/jnksoftware-portal/static/js/main.94335717.chunk.js"
+  },
+  {
+    "revision": "ee03f78a316423c865b8",
+    "url": "/jnksoftware-portal/static/js/runtime-main.3506da46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jnksoftware-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
